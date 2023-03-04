bed777ad1fa7501fa6d3235ca7108c17a6477969
Oksanahilyk <hilyk@ukr.net>
2023-03-04 07:32:50 +0200

    правка до завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -22,37 +22,6 @@ function createGalleryItemsMarkup(items) {
   }).join('');
 }
 
-galleryContainer.addEventListener('click', imageClick);
- function imageClick(event) {
-	event.preventDefault();
-	if (event.target.nodeName !== "IMG") return;
- 
-	const isItemImage = event.target.classList.contains('gallery__image');
-	if (!isItemImage) return;
- 
-	const currentImgUrl = event.target.dataset.source;
- 
-	const instance = basicLightbox.create(
-	  `<img src="${currentImgUrl}" width="1280" height="auto">`,
-	  {
-		 onShow: (instance) => {
-			window.addEventListener('keydown', onEscKeyPress);
-		 },
-		 onClose: (instance) => {
-			window.removeEventListener('keydown', onEscKeyPress);
-		 },
-	  }
-	);
-	instance.show();
- 
-	function onEscKeyPress(event) {
-	  const ESC_KEY_CODE = 'Escape';
-	  const isEscKey = event.code === ESC_KEY_CODE;
-	  if (!isEscKey) return;
-	  instance.close();
-	}
- }
-
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
 });
